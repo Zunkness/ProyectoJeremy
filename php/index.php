97a2dd9984ba5533d51856a7b182bdbf6da07550
Zunkness <jorgegaldar2@gmail.com>
2021-02-09 15:55:57 +0000

    dsa

--- php/index.php
+++ php/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">    
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/registro.css">
     <title>Inicio</title>
 </head>
 <body>
-    <button><a class="boton1" href="login.php">Acceder </a></button>
-	<button><a class="boton1" href="registro.php">Registrarse</a></button>
+  	<div id="contenido">
+      <button><a id="boton1" href="login.php">Acceder </a></button>
+  		<button><a id="boton2" href="registro.php">Registrarse</a></button>
+  	</div>
 </body>
 </html>
